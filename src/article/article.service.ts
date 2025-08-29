deffd0efc9d9dd03da9cec5fab5ee72caa71e518
amazingawwal <sakeebmawwal@gmail.com>
2025-08-29 18:51:50 +0100

    core: GET Unique by ID is done

--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -3,6 +3,7 @@ import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
 import { Article, Prisma } from 'generated/prisma';
 import { PrismaService } from 'src/prisma.service';
+import { error } from 'console';
 
 @Injectable()
 export class ArticleService {
@@ -13,14 +14,23 @@ export class ArticleService {
     return await this.prisma.article.create({data})
   }
 
-  async findAll() {
+  async findAll(): Promise<Article[]> {
     const articles = await this.prisma.article.findMany()
     console.log(articles)
     return articles
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} article`;
+  async findOne(id: number):Promise<Article> {
+
+    const result = await this.prisma.article.findUnique({
+      where:{
+        id
+      }
+    });
+    if (!result){
+      throw error;
+    }
+    return result
   }
 
   update(id: number, updateArticleDto: UpdateArticleDto) {
